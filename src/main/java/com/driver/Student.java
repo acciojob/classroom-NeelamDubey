9370b9b7347b83cc8453f36255d08d84187f2971
Neelam Dubey <ashidubey8208@gmail.com>
2023-05-20 18:30:17 +0530

    First try

--- src/main/java/com/driver/Student.java
+++ src/main/java/com/driver/Student.java
@@ -9,4 +9,26 @@ public class Student {
     public Student(){
 
     }
+
+    public Student(String name, int age, double averageScore) {
+        this.name = name;
+        this.age = age;
+        this.averageScore = averageScore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
